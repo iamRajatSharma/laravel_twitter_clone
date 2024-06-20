96b14ed96bfd1800b79c80b31ad6a188b82e01bc
iamrajatsharma <sharmarajat687@gmail.com>
2024-06-20 14:57:22 +0530

    list all ideas

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Idea;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('home');
+
+
+        // $idea = new Idea([
+        //     'content'=>'omes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. '
+        // ]);
+        // $idea->save();
+
+        $ideas = Idea::orderBy('created_at', 'DESC')->get();
+        return view('home', ['ideas'=> $ideas]);
     }
 }
